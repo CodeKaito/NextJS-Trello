2758154f15d56f48703248a04f04b7461458a2cb
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-08-28 18:01:37 +0200

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,12 +26,12 @@ function Header() {
                             <button type="submit" hidden>Search</button>
                     </form>
 
-                    <Avatar name='Jerome' round size='50' color="#0055D1" />
+                    <Avatar name='Jerome' round size='50' color="#0055D1" shadow-xl cursor-pointer />
                 </div>
             </div>
 
             <div className="flex items-center justify-center px-5 md:py-5">
-                <p className="flex items-center text-sm font-medium pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
+                <p className="flex items-center p-5 text-sm font-medium pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
                     <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
                     GPT is summarising your tasks for the day...
                 </p>

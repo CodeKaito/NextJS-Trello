d2e7a8ed73d654efc48d0f753b3940fe6c78bcf2
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-08-28 14:41:00 +0200

    update

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+function Header() {
+    return (
+        <header>
+            <Image
+                src="https://links.papareact.com/c2cdd5"
+                alt="trello"
+                width={300}
+                height={300}
+                className="w-44 md:w-56 pb-10 md:pd-0 object-contain"
+            />
+
+            <div>
+                
+            </div>
+        </header>
+    )
+}
+
+export default Header

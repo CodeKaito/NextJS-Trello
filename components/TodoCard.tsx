63efb69365f41376dfc7b436467c8df483ba2d09
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-08-30 14:26:46 +0200

    modified:   components/TodoCard.tsx

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -25,7 +25,16 @@ function TodoCard({
 }: Props) {
     return (
         <div className="bg-white rounded-md space-y-2 drop-shadow-md"
-            {...draggableProps} {...dragHandleProps} ref={innerRef}>hello</div>
+            {...draggableProps} {...dragHandleProps} ref={innerRef}>
+                <div className="flex justify-between items-center p-5">
+                    <p>{todo.title}</p>
+                    <button className="text-red-500 hover:text-red-700">
+                        <XCircleIcon className="ml-5 h-8 w-8" />
+                    </button>
+                </div>
+
+        { /* Add image here */ }
+        </div>
     )
 }
 

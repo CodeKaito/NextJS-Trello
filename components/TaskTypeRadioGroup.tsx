1d175cac0a997d29359b9e3258b7f5fcb13a2999
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-08-31 11:39:22 +0200

    update

--- components/TaskTypeRadioGroup.tsx
+++ components/TaskTypeRadioGroup.tsx
@@ -1,3 +1,24 @@
+const type = [
+    {
+        id: 'todo',
+        name: 'Todo',
+        description: 'A new task to be completed',
+        color: 'bg-red-500',
+    },
+    {
+        id: 'done',
+        name: 'Done',
+        description: 'A task that is currently being worked on',
+        color: 'bg-yellow-500',
+    },
+    {
+        id: 'done',
+        name: 'Done',
+        description: 'A task has been completed',
+        color: 'bg-green-500',
+    },
+]
+
 function TaskTypeRadioGroup() {
   return (
     <div></div>
--- components/dist/TaskTypeRadioGroup.js
+++ components/dist/TaskTypeRadioGroup.js
@@ -1,5 +1,25 @@
 "use strict";
 exports.__esModule = true;
+var type = [
+    {
+        id: 'todo',
+        name: 'Todo',
+        description: 'A new task to be completed',
+        color: 'bg-red-500'
+    },
+    {
+        id: 'done',
+        name: 'Done',
+        description: 'A task that is currently being worked on',
+        color: 'bg-yellow-500'
+    },
+    {
+        id: 'done',
+        name: 'Done',
+        description: 'A task has been completed',
+        color: 'bg-green-500'
+    },
+];
 function TaskTypeRadioGroup() {
     return (React.createElement("div", null));
 }
